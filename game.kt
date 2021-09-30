739ee79e0260f2dee89eb9487bb4a48964596525
Wig Cheng <onlywig@gmail.com>
2021-10-01 00:29:57 +0800

    add new skills and optimized game procedure

--- game.kt
+++ game.kt
@@ -8,19 +8,23 @@ fun main(){
   player[0].set_name("Wig", hero.skillType.NORMAL)
   player[1].set_name("Grace", hero.skillType.POISON)
 
-  println("Wig 目前血量 " + player[0].get_hp())
-  println("Grace 目前血量 " + player[1].get_hp())
+  println("Wig 目前血量 " + player[0].get_hp() + ", 魔力" + player[0].get_mp())
+  println("Grace 目前血量 " + player[1].get_hp() + ", 魔力" + player[1].get_mp())
 
   while( player[0].get_hp() > 0 && player[1].get_hp() > 0 ) {
-    if (turn == 0)
+    if (turn == 0) {
+      player[0].attack(player[1])
       turn = 1
-    else
+    } else {
+      player[1].attack(player[0])
       turn = 0
-
-    player[0].attack(player[1])
-    player[1].attack(player[0])
+    }
   }
 
-  println("勝利者為" + player[turn].getName())
+  if(turn == 1)
+    println("勝利者為" + player[0].getName())
+  else
+    println("勝利者為" + player[1].getName())
+
   println("=====遊戲結束=====")
 }
--- hero.kt
+++ hero.kt
@@ -10,7 +10,7 @@ class hero {
   enum class skillType {
     NORMAL,
     POISON,
-    WATERBALL
+    FIREGOD_GO
   }
 
   fun set_name(player_name: String, player_skill: skillType) {
@@ -22,7 +22,7 @@ class hero {
     when(player_skill){
       skillType.NORMAL -> temp_str = "一般物理攻擊"
       skillType.POISON -> temp_str = "毒咒"
-      else -> temp_str = "一般物理攻擊"
+      skillType.FIREGOD_GO -> temp_str = "火神-陽華突"
     }
 
     println("玩家" + this.name + "已進場, 自帶招式" + temp_str)
@@ -33,15 +33,31 @@ class hero {
     when(skill){
       skillType.NORMAL -> {
         println(name + "使用了物理攻擊")
-        target_hero.lostHP(5);
+        target_hero.lostHP(5)
       }
       skillType.POISON -> {
-        println(name + "使用了毒咒")
-        target_hero.lostHP(50);
+        if(get_mp() > 0) {
+          println(name + "使用了毒咒")
+          lostMP(50)
+          target_hero.lostHP(50)
+        } else {
+           println(name + "使用了毒咒,可是沒魔了啦！切換物理攻擊")
+           this.skill = skillType.NORMAL
+           attack(target_hero)
+           return
+        }
       }
-      else -> {
-        println(name + "使用了物理攻擊")
-        target_hero.lostHP(5);
+      skillType.FIREGOD_GO -> {
+        if(get_mp() > 0) {
+          println(name + "使用了火神-陽華突")
+          lostMP(100)
+          target_hero.lostHP(150)
+        } else {
+           println(name + "使用了火神-陽華突,可是沒魔了啦！切換物理攻擊")
+           this.skill = skillType.NORMAL
+           attack(target_hero)
+           return
+        }
       }
     }
     println(target_hero.name + "剩餘血量為" + target_hero.get_hp())
@@ -51,12 +67,27 @@ class hero {
     set_hp(get_hp() - lost_hp)
   }
 
+  fun lostMP(lost_mp: Int) {
+    set_mp(get_mp() - lost_mp)
+  }
+
   fun set_hp(hp: Int) {
     this.hp = hp
   }
 
+  fun set_mp(mp: Int) {
+    this.mp = mp
+  }
+
   fun getName(): String {
     return name
+  }
+
+  fun get_mp(): Int {
+    if (this.mp <= 0)
+      this.mp = 0
+
+    return mp
   }
 
   fun get_hp(): Int {
